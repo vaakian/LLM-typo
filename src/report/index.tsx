0de59ca960709399736560cd6028f50edaecc87d
vaakian <vaakian@gmail.com>
2025-05-01 02:52:03 +0800

    feat: update

--- src/report/index.tsx
+++ src/report/index.tsx
@@ -1,4 +1,16 @@
-// render report in an html file
-export const renderReport = async (typos: string[]) => {
+/**
+ * render report in an html file
+ *
+ * consists of:
+ * - the git diff content
+ * - display the diff with typos highlighted
+ */
+export const renderReport = async ({
+  diff,
+  typos,
+}: {
+  diff: string;
+  typos: string[];
+}) => {
   throw new Error('Not implemented');
 };
